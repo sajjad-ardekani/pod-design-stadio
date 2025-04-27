029f3fc98c333149ed7c150d3b7d63758476e14c
sajjad <sajjad.ardekani@gmail.com>
2025-04-27 22:11:17 +0330

    feat(docker) deployment

--- docker/images/files/config.js
+++ docker/images/files/config.js
@@ -1,2 +1,2 @@
-// Frontend configuration
-//var penpotFlags = "";
+var penpotPublicURI = "http://design.sweelux.com";
+var penpotFlags = "enable-login-with-oidc disable-login-with-password disable-onboarding disable-registration disable-login";
