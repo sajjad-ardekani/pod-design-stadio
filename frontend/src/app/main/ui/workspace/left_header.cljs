8f28214f9763c4f0234da1f40f23768be1c8120f
sajjad <sajjad.ardekani@gmail.com>
2025-08-04 23:18:49 +0330

    update

--- frontend/src/app/main/ui/workspace/left_header.cljs
+++ frontend/src/app/main/ui/workspace/left_header.cljs
@@ -91,7 +91,7 @@
         (dom/select-text! (mf/ref-val input-ref))))
 
     [:header {:class (dm/str class " " (stl/css :workspace-header-left))}
-     [:a {:href "https://app.podconverge.com/panel/projects"
+     [:a {:href "https://app.podconverge.com/panel/design-hub/projects"
           :class (stl/css :main-icon)} i/logo-icon]
      [:div {:alt (tr "workspace.sitemap")
             :class (stl/css :project-tree)}
